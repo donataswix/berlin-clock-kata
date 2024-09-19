42b902ae662b9a10fc37ac7e5e001dba46052fc0
UgneM <ugnem@wix.com>
2024-09-19 08:46:09 +0300

    Refactoring of functions

--- src/kata.mjs
+++ src/kata.mjs
@@ -1,42 +1,18 @@
 import * as rl from "node:readline/promises";
 
 const getSecondsLamp = (seconds) => (seconds % 2 === 0 ? "." : "X");
-const getFiveHoursLamps = (hours, interval, length, result = "") => {
-  if (hours < interval) {
+
+function getLamps(unit, interval, length, devider = undefined, result = "") {
+  if (unit < interval) {
     if (result.length === length) return result;
-    return getFiveHoursLamps(hours, interval, length, result + ".");
+    return getLamps(unit, interval, length, devider, result + ".");
   }
-  return getFiveHoursLamps(hours - interval, interval, length, result + "X");
-};
-const getOneHourLamps = (hours, interval, length, result = "") => {
-  if (hours < interval) {
-    if (result.length === length) return result;
-    return getOneHourLamps(hours, interval, length, result + ".");
+  if (devider && result.length % devider === devider - 1) {
+    return getLamps(unit - interval, interval, length, devider, result + "|");
   }
-  return getOneHourLamps(hours - interval, interval, length, result + "X");
-};
-const getFiveMinuteLamps = (minutes, interval, length, result = "") => {
-  if (minutes < interval) {
-    if (result.length === length) return result;
-    return getFiveMinuteLamps(minutes, interval, length, result + ".");
-  }
-  if (result.length % 3 === 2) {
-    return getFiveMinuteLamps(
-      minutes - interval,
-      interval,
-      length,
-      result + "|"
-    );
-  }
-  return getFiveMinuteLamps(minutes - interval, interval, length, result + "X");
-};
-const getOneMinuteLamps = (minutes, interval, length, result = "") => {
-  if (minutes < interval) {
-    if (result.length === length) return result;
-    return getOneMinuteLamps(minutes, interval, length, result + ".");
-  }
-  return getOneMinuteLamps(minutes - interval, interval, length, result + "X");
-};
+  return getLamps(unit - interval, interval, length, devider, result + "X");
+}
+
 function toBerlinClock(line) {
   const [
     decimalHours,
@@ -51,18 +27,20 @@ function toBerlinClock(line) {
   const hours = decimalHours * 10 + parseInt(hour);
   const minutes = decimalMinutes * 10 + parseInt(minute);
   const seconds = decimalSeconds * 10 + parseInt(second);
+
   return (
     getSecondsLamp(seconds) +
     " " +
-    getFiveHoursLamps(hours, 5, 4) +
+    getLamps(hours, 5, 4) +
     " " +
-    getOneHourLamps(hours % 5, 1, 4) +
+    getLamps(hours % 5, 1, 4) +
     " " +
-    getFiveMinuteLamps(minutes, 5, 11) +
+    getLamps(minutes, 5, 11, 3) +
     " " +
-    getOneMinuteLamps(minutes % 5, 1, 4)
+    getLamps(minutes % 5, 1, 4)
   );
 }
+
 for await (const line of rl.createInterface({ input: process.stdin })) {
   console.log(`${line} => ${toBerlinClock(line)}`);
 }
